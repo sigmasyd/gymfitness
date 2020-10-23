f1c2f3ae0f616584032357da55945250cd4dd986
Nathan <nathanbarcos12@gmail.com>
2020-10-23 10:50:34 -0500

    Se agrega seccion de bienvenidos
    	modificado:     wp-content/themes/gimfitness/front-page.php
    	modificado:     wp-content/themes/gimfitness/style.css

--- wp-content/themes/gimfitness/front-page.php
+++ wp-content/themes/gimfitness/front-page.php
@@ -1,5 +1,8 @@
 <?php get_header('front'); ?>
 
-
+<section class="bienvenida text-center seccion">
+  <h2 class="text-primario"><?php the_field('encabezado_bienvenida');?></h2>
+  <p><?php the_field('texto_bienvenida');?></p>
+</section>
 
 <?php get_footer(); ?>
