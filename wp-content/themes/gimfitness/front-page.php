acecb3653ba577a684341b2f85566bcaee79b8bf
Nathan <nathanbarcos12@gmail.com>
2020-10-25 02:04:08 -0500

    Se agrega custom types instructores, se crea widget para mostrarlo en la portada. Se aplica estilos
    	modificado:     wp-content/plugins/gymfitness_post_types/gymfitness_post_types.php
    	modificado:     wp-content/themes/gimfitness/front-page.php
    	modificado:     wp-content/themes/gimfitness/style.css

--- wp-content/themes/gimfitness/front-page.php
+++ wp-content/themes/gimfitness/front-page.php
@@ -51,4 +51,37 @@
       </div>
     </div>
   </section>
+
+  <section class="instructores">
+    <div class="contenedor seccion">
+      <h2 class="text-center texto-primario">Nuestros Instructores</h2>
+      <p class="text-center">Instructores profesionales que te ayudarán a lograr tus objetivos</p>
+      <ul class="listado-instructores">
+      <?php
+        $args = [
+          'post_type' => 'instructores',
+          'posts_per_page' => 30
+        ];
+        $instructores = new WP_Query($args);
+        while($instructores->have_posts()): $instructores->the_post();
+      ?>
+        <li class="instructor">
+          <?php the_post_thumbnail('mediano'); ?>
+          <div class="contenido text-center">
+            <h3><?php the_title(); ?></h3>
+            <?php the_content(); ?>
+            <div class="especialidad">
+            <?php 
+              $esp = get_field('especialidad');
+              foreach($esp as $e):
+            ?>
+              <span class="etiqueta"><?php print esc_html( $e ); ?></span>
+            <?php endforeach; ?>
+            </div>
+          </div>
+        </li>
+      <?php endwhile; wp_reset_postdata(); ?>
+      </ul>
+    </div>
+  </section>
 <?php get_footer(); ?>
