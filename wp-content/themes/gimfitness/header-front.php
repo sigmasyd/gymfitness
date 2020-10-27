95f7f038e6f922acc3e0fe9a769f1b6b7a0766c4
Carlos Barcos <fbarcos8@hotmail.com>
2020-10-26 20:56:09 -0500

    Escapando url al home para evitar cross-site scripting
    	modificado:     wp-content/themes/gimfitness/header-front.php
    	modificado:     wp-content/themes/gimfitness/header.php

--- wp-content/themes/gimfitness/header-front.php
+++ wp-content/themes/gimfitness/header-front.php
@@ -10,7 +10,7 @@
     <div class="contenedor header-grid">
       <div class="barra-navegacion">
         <div class="logo">
-          <a href="<?php print site_url(); ?>">
+          <a href="<?php print esc_url(site_url('/')); ?>">
             <img src="<?php print get_template_directory_uri(); ?>/img/logo.svg" alt="Logo sitio">
           </a>
         </div>
