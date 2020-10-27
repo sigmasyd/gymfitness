9a0ebe03fcfbc6e0dc066648ed21138e077eb81a
Carlos Barcos <fbarcos8@hotmail.com>
2020-10-26 21:14:04 -0500

    Se agrega css para dejar la barra fija. Se corigen errores de js
    	modificado:     wp-content/themes/gimfitness/js/scripts.js
    	modificado:     wp-content/themes/gimfitness/style.css

--- wp-content/themes/gimfitness/js/scripts.js
+++ wp-content/themes/gimfitness/js/scripts.js
@@ -5,27 +5,45 @@ jQuery(document).ready(($) => {
   });
 
   //Agregar Slider
-  $('.listado-testimoniales').bxSlider({
-    auto: true,
-    mode: 'fade',
-    controls: false
-  });
+  if($('.listado-testimoniales').length>0){
+    $('.listado-testimoniales').bxSlider({
+      auto: true,
+      mode: 'fade',
+      controls: false
+    });
+  }
 
   // Mapa de Leaflet
-  const lat = document.querySelector('#lat').value,
-        lng = document.querySelector('#lng').value,
-        direccion = document.querySelector('#direccion').value;
-
-  if(lat && lng && direccion){
-    var map = L.map('mapa').setView([lat,lng], 15);
+  if( document.querySelector('#lat') != null && document.querySelector('#lng') != null ){
+    const lat = document.querySelector('#lat').value,
+          lng = document.querySelector('#lng').value,
+          direccion = document.querySelector('#direccion').value;
   
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup(direccion)
-      .openPopup();
+    if(lat && lng && direccion){
+      var map = L.map('mapa').setView([lat,lng], 15);
+    
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    
+      L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup(direccion)
+        .openPopup();
+    }
   }
 
 });
+// agrega posicion fija en el header al hacer scroll
+window.onscroll = () => {
+  const scroll = window.scrollY;
+  console.log('scrolling ... ' , scroll);
+  const headerNav = document.querySelector('.barra-navegacion');
+  // si la cantidad de scroll es mayor a, agregar clase
+  if(scroll>300){
+    console.log('si');
+    headerNav.classList.add('fixed-top');
+  }else{
+    console.log('no');
+    headerNav.classList.remove('fixed-top');
+  }
+}
